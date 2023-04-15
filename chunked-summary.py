802ce40adf8cb313aebc193386595eb937138619
Felix Niemeyer <niemeyer.felix@gmail.com>
2023-04-15 02:15:39 +0200

    first person does not work

--- chunked-summary.py
+++ chunked-summary.py
@@ -16,24 +16,44 @@ parser.add_argument('-s', '--startswith', type=str, help='Read only files that s
 parser.add_argument('-t', '--target', type=str, help='Target directory for output files', required=True)
 
 parser.add_argument('-pp', '--prepromt', type=str, help='Prompt text before each chunk', required=False, 
-                    default='Please summarize the following text fragment:\n')
+                    default='Summarize the following text in first person.')
+
+parser.add_argument('-co', '--continuation', type=str, help='Prompt text before each chunk', required=False, 
+                    default='The following text is a continuation of the text you summarized before. Continue your summary in first person.')
+
 parser.add_argument('-cp', '--contextpromt', type=str, help='Target directory for output files', required=False, 
-                    default='Here is some context: ...')
+                    default='Here is what you have summarized previously: ')
 parser.add_argument('-ol', '--overlap', type=int, help='Number of chars to overlap', required=False, 
                     default=300)
 parser.add_argument('-cl', '--context', type=int, help='Number of chars from previous summary as context', 
                     required=False, default=300)
 parser.add_argument('-c', '--chunksize', type=int, help='Target chunk size in tokens', required=False, 
-                    default=3000)
+                    default=2500)
+# factor has to be less than 1
+parser.add_argument('-f', '--factor', type=int, help='Factor by which chatGPT should reduce by summary', required=False, 
+                    default=0.5)
 
 args = parser.parse_args()
 
+target_char_count = args.chunksize * CHARS_PER_TOKEN * args.factor 
+if(target_char_count < args.overlap):
+    print("Overlap has to be smaller than target summary size")
+    exit(1)
+
+target_word_count = target_char_count / 5
+
+if(args.factor < 0.05 or args.factor > 0.75):
+    print("Factor has to be between 0.05 and 0.5")
+    exit(1)
+
+condense_prompt = "Try to use around <wc> words and keep the summary in first person!"
+
+Path(args.target).mkdir(parents=True, exist_ok=True)
+
 openai.api_key = "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"
 
-startswith = "2023-03" # TODO: change to e.g. 2023-01
-
 def summarize(context, thoughts, outfile):
-    print("Making request")
+    print("Summarizing " + thoughts)
 
     messages = []
 
@@ -41,17 +61,28 @@ def summarize(context, thoughts, outfile):
         messages.append({
             "role": "system", "content": args.contextpromt + context
         }) 
+        messages.append({
+            "role": "system", "content": args.continuation
+        }) 
+    else:
+        messages.append({
+            "role": "system", "content": args.prepromt
+        }) 
 
     messages.append({
-        "role": "user", "content": args.prepromt + thoughts
+        "role": "system", "content": condense_prompt.replace("<wc>", str(target_word_count))
+    })
+
+    messages.append({
+        "role": "user", "content": thoughts
     }) 
+
+    print("Prompts: ", messages)
 
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=messages,
     )
-
-    print(response) 
 
     summary = response["choices"][0]["message"]["content"]
 
@@ -61,10 +92,11 @@ def summarize(context, thoughts, outfile):
         f.write(summary)
 
     return summary
-        
+
 thoughts = ""
-start_file = None
-current_file = None
+first_file = True
+start_file = "" 
+current_file = ""
 context = ""
 previous_outfile = None
 outfile_counter = 0
@@ -72,14 +104,15 @@ for filename in sorted(os.listdir(args.directory)):
     print(filename)
     if filename.startswith(args.startswith):
         print("processing")
-        if(start_file == None): 
+        if(first_file): 
+            first_file = False
             start_file = filename
         current_file = filename
         with open(args.directory + filename, "r") as f:
             print("reading " + filename)
             thoughts += f.read()
 
-            char_count = len(thoughts) + len(args.prepromt) + len(context) + args.overlap
+            char_count = len(thoughts) + len(args.prepromt) + len(context) + len(condense_prompt) + args.overlap
             if(len(context) > 0):
                 char_count += len(args.contextpromt)
 
@@ -87,7 +120,7 @@ for filename in sorted(os.listdir(args.directory)):
                 excess_chars = char_count - args.chunksize * CHARS_PER_TOKEN
                 chars = int(len(thoughts) - excess_chars)
 
-                outfile = args.target + start_file + "_" + current_file 
+                outfile = start_file + "_" + current_file 
                 if(outfile == previous_outfile):
                     outfile_counter += 1
                     outfile += "_" + str(outfile_counter)
@@ -103,4 +136,4 @@ for filename in sorted(os.listdir(args.directory)):
 
                 start_file = current_file
 
-summarize(context, thoughts, args.target + start_file + "_" + current_file)
+summarize(context, thoughts, start_file + "_" + current_file)

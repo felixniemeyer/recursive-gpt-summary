fd78207cebf88fbed07c41d5c62dbb772286b20a
Felix Niemeyer <niemeyer.felix@gmail.com>
2023-05-06 11:46:38 +0200

    setze v2 an im vue projekt

--- view/src/App.vue
+++ view/src/App.vue
@@ -30,11 +30,15 @@ async function init() {
 
 <template>
   <div id=container>
+    <div class="column arrow">▶</div>
     <TitleColumn />
     <TitleColumn />
     <TitleColumn />
     <TitleColumn />
+    <div class="column arrow">◀</div>
   </div>
+  <div id=top-fadein></div>
+  <div id=bottom-fadein></div>
 </template>
 
 <style scoped>
@@ -44,14 +48,40 @@ async function init() {
   height: 100vh;
 }
 
+#top-fadein, #bottom-fadein {
+  position: fixed; 
+  left: 0; 
+  height: 2rem; 
+  width: 100vh; 
+}
+
+#top-fadein {
+  top: 0; 
+  background: linear-gradient(to bottom, #ffff 20%, #fff0);
+}
+  
+#bottom-fadein {
+  bottom: 0; 
+  background: linear-gradient(to bottom, #fff0, #ffff 80%);
+}
+
 .column {
   flex: 0 0 auto; /* set flex-grow, flex-shrink and flex-basis to 0, so the boxes don't stretch */
-  width: 300px; /* set a fixed width for the boxes */
   margin-right: 10px; /* add margin between the boxes */
-  height: 100vh;
+  height: 100%;
   box-sizing: border-box;
   padding: 1rem; 
   overflow-y: scroll;
+  align-items: center;
+}
+
+.arrow {
+  font-size: 3rem;
+  line-height: 100vh;
+  overflow-y: hidden;
+  color: #888; 
+  padding: 2rem; 
+  user-select: none;
 }
 
 </style>
--- view/src/components/TitleColumn.vue
+++ view/src/components/TitleColumn.vue
@@ -4,22 +4,26 @@ import config from '../config'
 </script>
 
 <template>
-  <div class=column>
+  <div class="column arrow">▶</div>
+  <div class=column id=title-page>
     <h1> {{ config.title }} </h1>
     <h3> {{ config.subtitle }} </h3>
-    <p> {{ config.description }} </p>
     <p> {{ config.description }} </p>
     <p>
       Dieser Text wurde mit dem Tool <i>recursive-gpt-summary</i> auf basis von ChatGPT zusammengefasst um ein leichtes und schnelles Erkunden zu ermöglichen. 
     </p>
     <p>
-      Ihr können dieses Tool und auch diese WebApp selbst verwenden, es ist Open Source:
+      Ihr können dieses Tool und auch diese WebApp selbst verwenden, es ist open source:
       <a href="https://github.com/felixniemeyer/recursive-gpt-summary" target="_blank">
         Recursive GPT Summary auf Github
       </a>
     </p>
   </div>
+  <div class="column arrow">◀</div>
 </template>
 
 <style scoped>
+#title-page {
+  width: max(50vw, 50vh);
+}
 </style>
